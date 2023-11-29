0418ee30009af2d109a1b4298b6fe0f3b970ed6c
nunqi <61606327+nunqi@users.noreply.github.com>
2023-11-29 15:04:11 -0300

    Initial upload

---
+++ server.js
@@ -0,0 +1,19 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    console.log('new connection');
+
+    ws.on('error', console.error);
+
+    ws.on('message', (rawData) => {
+        let data = JSON.parse(rawData)
+        console.log(`${data.author}: ${data.message}`);
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                ws.send(rawData.toString());
+            }
+        });
+    });
+})
